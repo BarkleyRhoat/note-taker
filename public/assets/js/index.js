c82ecde06feb071f1b7927e36f4299ab48241fbd
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-12-14 17:05:24 -0500

    added server file

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -192,3 +192,6 @@ if (window.location.pathname === '/notes') {
 }
 
 getAndRenderNotes();
+
+
+
---
+++ public/assets/js/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static("public"));
+
+const apiRouter = require("./routes/apiRoutes");
+const htmlRouter = require("./routes/htmlRoutes");
+
+app.use("/api", apiRouter);
+
+app.use("/", htmlRouter);
+
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost:${PORT}`)
+);
